cef666096103c1932b134cbb10ae55b0a73924fd
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-08-09 09:34:55 +0530

    Routes Created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Root from "./layouts/Root";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
-    <>
-  <Root/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
-export default App
+export default App;
